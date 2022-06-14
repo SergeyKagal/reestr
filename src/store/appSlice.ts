d5e04e32d6cb2ce2e802125800fd01c55033d466
SergeyKagal <3461995@gmail.com>
2022-06-14 19:44:00 +0300

    init: initial commit

---
+++ src/store/appSlice.ts
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const appSlice = createSlice({
+  name: 'store',
+  initialState,
+  reducers: {},
+});
+
+// export const {} = todoSlice.actions;
+export default appSlice.reducer;
