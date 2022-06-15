9bc793b6a3b4b09240e702e958b8167742f47a44
SergeyKagal <3461995@gmail.com>
2022-06-15 15:27:06 +0300

    feat: page++ --

--- src/components/pages/Main/Reestr/ReestrPagination.tsx
+++ src/components/pages/Main/Reestr/ReestrPagination.tsx
@@ -1,30 +1,53 @@
 import React, { useState } from 'react';
 import { reestrLength } from '../../../../constants/mocks/reest-data-mock';
 
-const pageNumsInitial = [1, 2, 3, 4, 5, 6, reestrLength - 1, reestrLength];
+const pageNumsInitial = [0, 1, 2, 3, 4, 5, reestrLength - 1, reestrLength];
 
 export const ReestrPagination = () => {
-  const [currentPageNum, setCurrentPageNum] = useState(0);
-
   const [pageNums, setPagenums] = useState<number[]>(pageNumsInitial);
+
+  const incrementPage = () => {
+    setPagenums(
+      pageNums.map((pageNum, i) => {
+        if (i < 6) {
+          return pageNum + 1;
+        } else {
+          return pageNum;
+        }
+      })
+    );
+  };
+  const decrementPage = () => {
+    if (pageNums[0] > 0) {
+      setPagenums(
+        pageNums.map((pageNum, i) => {
+          if (i < 6) {
+            return pageNum - 1;
+          } else {
+            return pageNum;
+          }
+        })
+      );
+    }
+  };
 
   return (
     <div className="pagination">
-      <button className="button-32 left"></button>
+      <button className="button-32 left" onClick={decrementPage}></button>
       <button className="button-32 left-double"></button>
       <div className="pages-num-wrapper">
         {pageNums.map((pageNum, index) => {
           if (!index) {
             return (
               <button className="pagination-num-button current-num" key={pageNum} disabled={true}>
-                {pageNum}
+                {pageNum + 1}
               </button>
             );
           }
           if (index !== 5) {
             return (
               <button className="pagination-num-button" key={pageNum}>
-                {pageNum}
+                {pageNum + 1}
               </button>
             );
           } else {
@@ -38,7 +61,7 @@ export const ReestrPagination = () => {
       </div>
 
       <button className="button-32 right-double"></button>
-      <button className="button-32 right"></button>
+      <button className="button-32 right" onClick={incrementPage}></button>
     </div>
   );
 };
